12ba3cbdf1d85f128be3ad2525b591d252fe74e3
Oskar Radon <oskarradonkimball@gmail.com>
2017-11-02 15:39:13 -0700

    nav js not working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,30 @@
 $(function(){
-  // if menu div is not open
-  // when you click on menu icon
-  // show menu div
-    // animate? - this could be in css, transition property
-  // otherwise hide menu div
+  // if mobile vw
+  if (window.innerWidth <= 800) {
+
+    // if menu div is not open
+    if ($('nav').is(':visible')) {
+
+      // when you click on menu icon
+      $('.header__menu-icon').click(() => {
+
+        // show menu div
+        $('nav').hide();
+        // animate? - this could be in css, transition property
+
+      });
+
+    } else {
+
+      $('.header__menu-icon').click(() => {
+
+        // otherwise hide menu div
+        $('nav').show();
+
+      });
+
+    }
+
+  }
+
 })
