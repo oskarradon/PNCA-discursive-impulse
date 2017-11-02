a0b7d76de46ca61976c61c18cc4c6b5d33302ebc
Oskar Radon <oskarradonkimball@gmail.com>
2017-11-02 15:45:46 -0700

    i love jquery :-P

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,29 +2,9 @@ $(function(){
   // if mobile vw
   if (window.innerWidth <= 800) {
 
-    // if menu div is not open
-    if ($('nav').is(':visible')) {
-
-      // when you click on menu icon
-      $('.header__menu-icon').click(() => {
-
-        // show menu div
-        $('nav').hide();
-        // animate? - this could be in css, transition property
-
-      });
-
-    } else {
-
-      $('.header__menu-icon').click(() => {
-
-        // otherwise hide menu div
-        $('nav').show();
-
-      });
-
-    }
-
+    $('.header__menu-icon').click(() => {
+      $('nav').slideToggle('slow');
+    });
   }
 
-})
+});
