22948d1f4173621252547892d65e2de727cbfe3b
Oskar Radon <oskarradonkimball@gmail.com>
2017-11-13 10:19:12 -0800

    fix footer right for when scrollbar visible

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,4 +6,18 @@ $(function(){
     });
   }
 
+  function checkScrollBars() {
+      var b = $('body');
+      var normalw = 0;
+      var scrollw = 0;
+      console.log('here');
+      if (!($("body").height() > $(window).height())) {
+        console.log('inside if');
+        console.log('hello');
+        $('footer').css({right:'62.12px'});
+      }
+  }
+
+  checkScrollBars();
+
 });
